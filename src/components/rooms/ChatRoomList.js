bb89c4449f5c935f24d1425149eb16bc16aa72b8
avikkr75 <avikkr75@gmail.com>
2021-04-29 14:17:45 +0530

    displayed chat room

--- src/components/rooms/ChatRoomList.js
+++ src/components/rooms/ChatRoomList.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { Nav } from 'rsuite';
+import { Link, useLocation } from 'react-router-dom';
+import { Loader, Nav } from 'rsuite';
+import { useRooms } from '../../context/rooms.context';
 import RoomItem from './RoomItem';
 
 const ChatRoomList = ({ aboveElHeight }) => {
+  const rooms = useRooms();
+  const location = useLocation();
+
   return (
     <Nav
       appearance="subtle"
@@ -12,10 +17,21 @@ const ChatRoomList = ({ aboveElHeight }) => {
       style={{
         height: `calc(100% - ${aboveElHeight}px)`,
       }}
+      activeKey={location.pathname}
     >
-      <Nav.Item>
-        <RoomItem />
-      </Nav.Item>
+      {!rooms && <Loader center vertical content="Loading..." size="md" />}
+      {rooms &&
+        rooms.length > 0 &&
+        rooms.map(room => (
+          <Nav.Item
+            componentClass={Link}
+            to={`/chat/${room.id}`}
+            key={room.id}
+            eventKey={`/chat/${room.id}`}
+          >
+            <RoomItem room={room} />
+          </Nav.Item>
+        ))}
     </Nav>
   );
 };
