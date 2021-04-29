ebc74d6e1d6f544e3bbf55e3fa810c120742963a
avikkr75 <avikkr75@gmail.com>
2021-04-29 14:50:22 +0530

    created layout for home page and created chat page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import 'rsuite/dist/styles/rsuite-default.css';
 import PrivateRoute from './components/PrivateRoute';
 import PublicRoute from './components/PublicRoute';
 import { ProfileProvider } from './context/profile.context';
-import Home from './pages/Home';
+import Home from './pages/Home/index';
 import SignIn from './pages/SignIn';
 import './styles/main.scss';
 
