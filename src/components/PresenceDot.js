59c7760c74eec24164a078be4e70399681e6dc23
avikkr75 <avikkr75@gmail.com>
2021-05-08 18:17:43 +0530

    displayed real-time presence

---
+++ src/components/PresenceDot.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Badge, Tooltip, Whisper } from 'rsuite';
+import { usePresence } from '../misc/custom-hooks';
+
+const getColor = presence => {
+  if (!presence) {
+    return 'gray';
+  }
+
+  switch (presence.state) {
+    case 'online':
+      return 'green';
+    case 'offline':
+      return 'red';
+    default:
+      return 'gray';
+  }
+};
+
+const getText = presence => {
+  if (!presence) {
+    return 'Unknown State';
+  }
+
+  return presence.state === 'online'
+    ? 'Online'
+    : `Last seen ${new Date(presence.last_changed).toLocaleDateString()}`;
+};
+
+const PresenceDot = ({ uid }) => {
+  const presence = usePresence(uid);
+  return (
+    <Whisper
+      placement="top"
+      trigger="hover"
+      speaker={<Tooltip>{getText(presence)}</Tooltip>}
+    >
+      <Badge
+        className="cursor-pointer"
+        style={{ background: getColor(presence) }}
+      />
+    </Whisper>
+  );
+};
+
+export default PresenceDot;
--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TimeAgo from 'timeago-react';
+import PresenceDot from '../../PresenceDot';
 import ProfileAvatar from '../../ProfileAvatar';
 import ProfileInfoBtnModal from './ProfileInfoBtnModal';
 
@@ -9,6 +10,8 @@ const MessageItem = ({ message }) => {
   return (
     <li className="padded mb-1">
       <div className="d-flex align-items-center font-bolder mb-1">
+        <PresenceDot uid={author.uid} />
+
         <ProfileAvatar
           src={author.avatar}
           name={author.name}
